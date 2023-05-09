47106145adb491cd1d03212109dc2d0070dd99e9
haticeoter <123317727+haticeoter@users.noreply.github.com>
2023-05-09 16:06:27 +0300

    code added

--- imdb_movies.py
+++ imdb_movies.py
@@ -1,17 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.imdb.com/chart/top/"
-
+url = "http://www.imdb.com/chart/top"
 response = requests.get(url)
-
 html_content = response.content
 soup = BeautifulSoup(html_content,"html.parser")
-titles = soup.find_all("td",{"class" : "titleColumn"})
-ratings =  soup.find_all("td",{"class" : "ratingColumn imdbRating"})
 
-for title,rating in zip(titles,ratings):
+a = float(input("Enter the rating:"))
+
+titles = soup.find_all("td",{"class":"titleColumn"})
+ratings = soup.find_all("td",{"class","ratingColumn imdbRating"})
+
+for title, rating in zip(titles,ratings):
     title = title.text
     rating = rating.text
-    print("Titles", title)
-    print("Rating", rating)
+
+    title = title.strip()
+    title = title.replace("\n","")
+
+    rating = rating.strip()
+    rating = rating.replace("\n","")
+
+    if (float(rating) > a):
+        print("Film name: {} Film ratingi : {}".format(title,rating))
